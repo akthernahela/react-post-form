fa67b020f138003668e5f492b7c62b3cfc28bcdc
akthernahela <nahela00@gmail.com>
2025-11-13 18:00:48 +0100

    Add author to the form data

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 function App() {
   const linkEndpoint = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts'
   const [formData, setFormData] = useState({
-    name: "",
+    author: "",
     title: "",
     body: "",
     public: false,
