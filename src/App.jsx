e95e059651807c9bfa1f10d923f426a386be8f86
akthernahela <nahela00@gmail.com>
2025-11-13 17:52:12 +0100

    Update the class name

--- src/App.jsx
+++ src/App.jsx
@@ -29,18 +29,18 @@ function App() {
     <>
       <h1>Apri un ticket</h1>
       <form>
-        <div class="row g-5">
-          <div class="col">
-            <label for="inputAuthor" class="form-label">Author</label>
-            <input type="text" class="form-control" placeholder="Author name" aria-label="Author" />
+        <div className="row g-5">
+          <div className="col">
+            <label className="form-label">Author</label>
+            <input type="text" className="form-control" placeholder="Author name" aria-label="Author" />
           </div>
-          <div class="col">
-            <label for="inputTitle" class="form-label">Title</label>
-            <input type="text" class="form-control" placeholder="Title" aria-label="Title" />
+          <div className="col">
+            <label className="form-label">Title</label>
+            <input type="text" className="form-control" placeholder="Title" aria-label="Title" />
           </div>
-          <div class="col mb-3">
-            <label for="Textarea" class="form-label">Body</label>
-            <textarea class="form-control" id="Textarea" rows="3"></textarea>
+          <div className="col mb-3">
+            <label className="form-label">Body</label>
+            <textarea className="form-control" id="Textarea" rows="3"></textarea>
           </div>
         </div>
         <button type="submit">Submit</button>
